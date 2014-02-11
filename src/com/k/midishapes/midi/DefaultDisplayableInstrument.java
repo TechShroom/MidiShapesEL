ff5a6b42378519cc327a407ac27e296b7f6c51e7
Kenzie Togami <ket1999@gmail.com>
2014-02-10 20:36:28 -0800

    Doc DDI

--- src/com/k/midishapes/midi/DefaultDisplayableInstrument.java
+++ src/com/k/midishapes/midi/DefaultDisplayableInstrument.java
@@ -9,14 +9,38 @@ import emergencylanding.k.library.lwjgl.tex.ELTexture;
 
 public abstract class DefaultDisplayableInstrument implements
         DisplayableInstrument {
+    /**
+     * The constant representing the number of notes a MIDI supports
+     */
     protected static final int NOTES = 128; // 128 notes
+    /**
+     * A default 'on' texture
+     */
     protected static final ELTexture noteOnStatic = new ColorTexture(Color.RED);
+    /**
+     * A default 'off' texture
+     */
     protected static final ELTexture noteOffStatic = new ColorTexture(
             Color.DARK_GRAY);
+    /**
+     * The 'on' note color
+     */
     protected ELTexture noteOn = noteOnStatic;
+    /**
+     * The 'off' note color
+     */
     protected ELTexture noteOff = noteOffStatic;
+    /**
+     * The id of this instrument
+     */
     protected final int id;
 
+    /**
+     * Sets the id for this instrument
+     * 
+     * @param idChannel
+     *            - the id to use. Useful for positioning.
+     */
     public DefaultDisplayableInstrument(int idChannel) {
         id = idChannel;
     }
@@ -37,10 +61,22 @@ public abstract class DefaultDisplayableInstrument implements
         return noteOff;
     }
 
+    /**
+     * Sets the note on color.
+     * 
+     * @param t
+     *            - the new {@link ELTexture} to use as 'on'
+     */
     public void setNoteOn(ELTexture t) {
         noteOn = t;
     }
 
+    /**
+     * Sets the note off color.
+     * 
+     * @param t
+     *            - the new {@link ELTexture} to use as 'off'
+     */
     public void setNoteOff(ELTexture t) {
         noteOff = t;
     }
